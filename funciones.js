51b018fef9f0ae78002ab622f4999d0d3f52a34a
eliasibg <eliasibgon@hotmail.com>
2020-12-01 14:35:53 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,18 +1,29 @@
 function Buscar(){
 
     var settings = {
-        "url": "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-          "x-rapidapi-key": "2075d24590msh3bd1515205ed040p1b4228jsn4fa5c1e186f9",
-          "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
-        },
-      };
+  "url": "https://grupobimbo--test.my.salesforce.com/services/oauth2/token",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Cookie": "BrowserId=6AzT9jP5EeuUqxkX4XlE_A"
+  },
+  "data": {
+    "username": "integra@pilotobimbo.com.ricolino",
+    "password": "Nu3v0P4nB1mb0.Rf0u4DTB3a5LdVKL9M5Jv9uY",
+    "grant_type": "password",
+    "client_id": "3MVG9FG3dvS828gL.pckzEFAegT7jjHRlbDeOzj.AqkoSXBxU_7HrPGr2Ne7zeQgWGI.haAT4jR0FvOZbTk.G",
+    "client_secret": "B4F6F1751E52DF588F365714085E589B35045ECA95E016C39BC193BD9DECFE48"
+  }
+};
       
       $.ajax(settings).done(function (response) {
         console.log(response);
-        $("#contenido").text(response.value);
+          
+          var bear = response.access_token;
+          
+          
+        $("#contenido").text(bear);
       });
 
 
