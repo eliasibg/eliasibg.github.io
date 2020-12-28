a6c09625eb71040767b8fad2af4464a1f383c21c
eliasibg <eliasibgon@hotmail.com>
2020-12-28 15:24:51 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,47 +1,14 @@
-function Buscar(){
-/*
-    var settings = {
-        "url": "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-          "x-rapidapi-key": "2075d24590msh3bd1515205ed040p1b4228jsn4fa5c1e186f9",
-          "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
-        },
-      };
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        $("#contenido").text(response.value);
-      });
-*/
+function buscarID(){
+
     
-    
-    
-    lpTag.agentSDK.init({
-    
-        
-        
-        
-    });
+    $("#cardIDCliente").text("123456");
+    $("#cardNombreCliente").text("Juan Perez");
+    $("#cardIDPrecio").text("999999");
+    $("#cardDescPrecio").text("Test Price");
+    $("#cardCorreo").text("test@test.com");
+    $("#cardTelefonos").text("55-55-55-55-55");
 
-
-
-    var onSuccess = function(data) {
-        $("#alerta").text("exito");
-    };
-
-    var onError = function(err) {
-       $("#alerta").text("error");
-    };
-
-    var pathToData = "visitorInfo.visitorName";
-
-    lpTag.agentSDK.get(pathToData, onSuccess, onError);
     
     
     
 }
-
-
-    
